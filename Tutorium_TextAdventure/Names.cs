13b65265fdf324014cf0cd64187df94c2551e547
Patrick Pleumann <patrickpleumann@web.de>
2025-09-19 15:43:25 +0200

    names

---
+++ Tutorium_TextAdventure/Names.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorium_TextAdventure
+{
+    public class Names
+    {
+        List<string> prefix = new List<string> { "", "", "" };
+        List<string> names = new List<string> { "", "", "" };
+        List<string> suffix = new List<string> { "", "", "" };
+
+
+        List<string> RoomDescription = new List<string>();
+    }
+}
